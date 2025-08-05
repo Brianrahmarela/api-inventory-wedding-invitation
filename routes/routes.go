b960bf31e2112411194fcc86d3117d490dde0cfb
Brian <brian.rahmarela@gmail.com>
2025-08-05 14:41:17 +0700

    tes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,21 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRoutes(router *gin.Engine, db *gorm.DB) {
+	// Grup /api
 	api := router.Group("/api")
 	{
+		// Endpoint test: /api/about
+		api.GET("/about", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello")
+		})
+
 		SetupAuthRoutes(api, db)
 		// SetupUserRoutes(api, db)
 	}
